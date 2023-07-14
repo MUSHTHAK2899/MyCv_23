e2c07b7c51205ca565542b6f57f958e3ba241930
mushthak2899 <mushthak2899@gmail.com>
2023-07-14 10:45:05 +0530

    d

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
-// module.exports = {
-//   plugins: [
-//   require('postcss-import'),
-//   require('tailwindcss/nesting')(require('postcss-nesting')),
-//   require('autoprefixer'),
-//   require('tailwindcss'),
-//   ]
-//   }
+module.exports = {
+  plugins: [
+  require('postcss-import'),
+  require('tailwindcss/nesting')(require('postcss-nesting')),
+  require('autoprefixer'),
+  require('tailwindcss'),
+  ]
+  }
